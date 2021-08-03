dad05677db65a03310001b1fbe650a02e3eba58a
Mohamed\ Sharaf <mohamedsharf26797@gmail.com>
2021-08-03 23:01:54 +0200

    simple APIs created

---
+++ APIs/server.js
@@ -0,0 +1,75 @@
+const express = require('express');
+const app = express();
+const PORT = process.env.PORT || 8000;
+const cors = require('cors');
+
+app.use(cors());
+app.use(express.json());
+
+app.get('/columns', getColumns)
+app.post('/columns', getData)
+
+
+
+
+app.listen(PORT, (err) => {
+    if (err) console.error(err);
+    console.log(`App server is running and listening on port ${PORT}`);
+});
+
+const columns = [
+    {
+        "name": "Product",
+        "function": "dimension"
+    },
+    {
+        "name": "Year",
+        "function": "dimension"
+    },
+    {
+        "name": "Country",
+        "function": "dimension"
+    },
+    {
+        "name": "Cost",
+        "function": "measure"
+    },
+    {
+        "name": "Revenue",
+        "function": "measure"
+    },
+    {
+        "name": "Units sold",
+        "function": "measure"
+    },
+]
+
+
+const data = {
+    Product: ["Diskette", "Memory Card", "HDTV Tuner", "Flat Panle Graphics Monitor", "Digital Camera", "Minitower Speaker"],
+    Cost: [333.08, 7.07, 10.77, 194.76, 13.18, 143.3]
+}
+
+
+
+
+
+
+function getColumns(req, res) {
+    res.json(columns);
+}
+function getData(req, res) {
+    const { measure } = req.body;
+    const { dimension } = req.body
+
+    let result = [
+        {
+            "name": dimension,
+            "values": data[dimension]
+        }, {
+            "name": measure[0],
+            "values": data[measure[0]]
+        }
+    ]
+    res.json(result);
+}
